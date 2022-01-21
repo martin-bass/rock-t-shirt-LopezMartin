c817218b210343ae4b7154891258e11ef6e39f3e
martin-bass <martin_reym@hotmail.com>
2022-01-21 19:46:39 -0300

    cambios en NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 //Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/js/dist/dropdown'
+import 'bootstrap/js/dist/dropdown';
 
 import logo_mano from '/Users/macbookpro/Desktop/CODERHOUSE/Curso_ReactJS/T-Shirt-Rock-Store_LopezMartin/rock-t-shirt/src/img/logo_mano.png';
+
 import './NavBar.css';
 
 function NavBar() {
@@ -37,6 +38,13 @@ function NavBar() {
                         <a className="nav-link" href="#">CONTACT</a>
                     </li>
                 </ul>
+                <div className='login-container'>
+                    <h5>LogIn</h5>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                        <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                    </svg>
+                </div>
             </div>
         </nav>
     );
